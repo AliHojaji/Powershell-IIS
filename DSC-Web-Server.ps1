4bd6795b8dd78bb4e220711a19eb3a6a41379729
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 14:13:30 +0330

    Add files via upload

---
+++ DSC-Web-Server.ps1
@@ -0,0 +1,24 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*-----------------*--#
+#---> Configure IIS <---#
+#--*-----------------*--#
+
+#--> configuration
+Configuration IISConfig {
+
+    #resource
+    WindowsFeature IIS {
+
+    Ensure = "Present"
+    Name = "Web-Server"
+
+   }
+
+}
+
+#--> create the configuration (.mof)
+IISConfig -ComputerName WEB-Test -OutputPath c:\nuggetlab
+
+#--> push the configuration to WEB-NUG
+Start-DscConfiguration -Path c:\nuggetlab -Wait -Verbose
